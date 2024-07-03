fa9ed167ee4b4d82804e5b591ccf493c763da43b
Paxagi <paxagi@gmail.com>
2024-07-03 17:01:18 +0300

    fix: add TasksRepositoryImpl

--- app/src/main/java/com/example/cleanarchitectureshowcase/features/home/domain/task/TasksRepositoryImpl.kt
+++ app/src/main/java/com/example/cleanarchitectureshowcase/features/home/domain/task/TasksRepositoryImpl.kt
@@ -2,16 +2,17 @@ package com.example.cleanarchitectureshowcase.features.home.domain.task
 
 import com.example.cleanarchitectureshowcase.features.home.data.TaskDTO
 import com.example.cleanarchitectureshowcase.features.home.domain.PriorityLevel
+import java.util.LinkedList
 import java.util.UUID
 
 class TasksRepositoryImpl : TasksRepository {
-    private val taskStorage: MutableList<TaskDTO> = mutableListOf(
+    private val taskStorage: LinkedList<TaskDTO> = listOf(
         TaskDTO(UUID.randomUUID(), "one", PriorityLevel.HIGH, null, false),
         TaskDTO(UUID.randomUUID(), "two", null, null, false),
         TaskDTO(UUID.randomUUID(), "3", PriorityLevel.LOW, null, false),
         TaskDTO(UUID.randomUUID(), "four", null, null, false),
         TaskDTO(UUID.randomUUID(), "five", null, null, false),
-    )
+    ).let { LinkedList<TaskDTO>(it) }
 
     override suspend fun setTask(task: TaskDTO): TaskDTO {
         taskStorage.add(task)
@@ -19,21 +20,24 @@ class TasksRepositoryImpl : TasksRepository {
     }
 
     override suspend fun updateTask(task: TaskDTO): TaskDTO {
-        TODO("Not yet implemented")
+        taskStorage[indexByID(task.id)] = task
+        return getTask(task.id)
     }
 
     override suspend fun getTask(id: UUID): TaskDTO {
-        TODO("Not yet implemented")
+        return taskStorage[indexByID(id)]
     }
 
     override suspend fun getTasks(): List<TaskDTO> = taskStorage
 
     override suspend fun deleteTask(id: UUID) {
-        TODO("Not yet implemented")
+        taskStorage.removeAt(indexByID(id))
     }
 
-    suspend fun findTask(id: UUID): TaskDTO {
-        TODO("Not yet implemented")
+    private fun indexByID(id: UUID): Int {
+        taskStorage.let { tasks ->
+            return tasks.indexOf(tasks.find { it.id == id })
+        }
     }
 
 }
