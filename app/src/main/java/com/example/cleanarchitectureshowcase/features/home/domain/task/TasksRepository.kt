7b28918727f298e62657a8f109f0cd682834405e
paxagi <paxagi@gmail.com>
2024-07-06 21:27:52 +0300

    fix: change interfaces of TaskService and it realisation

--- app/src/main/java/com/example/cleanarchitectureshowcase/features/home/domain/task/TasksRepository.kt
+++ app/src/main/java/com/example/cleanarchitectureshowcase/features/home/domain/task/TasksRepository.kt
@@ -4,9 +4,9 @@ import com.example.cleanarchitectureshowcase.features.home.data.TaskDTO
 import java.util.UUID
 
 interface TasksRepository {
-    suspend fun setTask(task: TaskDTO): TaskDTO
-    suspend fun updateTask(task: TaskDTO): TaskDTO
-    suspend fun getTask(id: UUID): TaskDTO
+    suspend fun setTask(task: TaskDTO)
+    suspend fun updateTask(task: TaskDTO)
+    suspend fun getTask(id: UUID): TaskDTO?
     suspend fun getTasks(): List<TaskDTO>
     suspend fun deleteTask(id: UUID)
 }
--- app/src/main/java/com/example/cleanarchitectureshowcase/features/home/domain/task/TasksRepositoryImpl.kt
+++ app/src/main/java/com/example/cleanarchitectureshowcase/features/home/domain/task/TasksRepositoryImpl.kt
@@ -14,30 +14,21 @@ class TasksRepositoryImpl : TasksRepository {
         TaskDTO(UUID.randomUUID(), "five", null, null, false),
     ).let { LinkedList<TaskDTO>(it) }
 
-    override suspend fun setTask(task: TaskDTO): TaskDTO {
+    override suspend fun setTask(task: TaskDTO) {
         taskStorage.add(task)
-        return task
     }
 
-    override suspend fun updateTask(task: TaskDTO): TaskDTO {
-        taskStorage[indexByID(task.id)] = task
-        return getTask(task.id)
+    override suspend fun updateTask(task: TaskDTO) {
+        var linkToUpdatedTask = taskStorage.find { it.id == task.id }
+        linkToUpdatedTask = task
     }
 
-    override suspend fun getTask(id: UUID): TaskDTO {
-        return taskStorage[indexByID(id)]
-    }
+    override suspend fun getTask(id: UUID): TaskDTO? = taskStorage.firstOrNull { it.id == id }
 
     override suspend fun getTasks(): List<TaskDTO> = taskStorage
 
     override suspend fun deleteTask(id: UUID) {
-        taskStorage.removeAt(indexByID(id))
-    }
-
-    private fun indexByID(id: UUID): Int {
-        taskStorage.let { tasks ->
-            return tasks.indexOf(tasks.find { it.id == id })
-        }
+            taskStorage.removeAt(taskStorage.indexOfFirst { it.id == id })
     }
 
 }
