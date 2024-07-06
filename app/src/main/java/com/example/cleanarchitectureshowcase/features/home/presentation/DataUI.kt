1cc9bfccd728d64777bd0292feaaa6d72c6d6bce
paxagi <paxagi@gmail.com>
2024-07-06 21:34:26 +0300

    fix: delete DataUI (unused)

--- app/src/main/java/com/example/cleanarchitectureshowcase/features/home/presentation/DataUI.kt
+++ app/src/main/java/com/example/cleanarchitectureshowcase/features/home/presentation/DataUI.kt
@@ -3,12 +3,6 @@ package com.example.cleanarchitectureshowcase.features.home.presentation
 import com.example.cleanarchitectureshowcase.features.home.domain.PriorityLevel
 import java.time.LocalDate
 
-data class DataUI(
-    val title: String,
-    val subTitle: String,
-    val description: String,
-)
-
 data class TaskUI(
     val header: String,
     val priority: PriorityLevel?,
